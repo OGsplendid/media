d357da50c39af4aa931b47c0ed39f4ba748d70cd
AppVeyor <ci@appveyor.com>
2023-09-30 16:09:28 +0300

    undone

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,18 +6,18 @@ export default class App {
     this.memory = new Memory();
     this.form = this.element.querySelector('.form');
     this.input = this.element.querySelector('.input');
-    this.audioButton = this.element.querySelector('.audio-button');
-    this.videoButton = this.element.querySelector('.video-button');
+    // this.audioButton = this.element.querySelector('.audio-button');
+    // this.videoButton = this.element.querySelector('.video-button');
     // this.mediaStart = this.element.querySelector('.media-start');
     // this.mediaStop = this.element.querySelector('.media-stop');
 
     this.onSubmit = this.onSubmit.bind(this);
-    this.onAudio = this.onAudio.bind(this);
+    // this.onAudio = this.onAudio.bind(this);
     // this.onVideo = this.onVideo.bind(this);
     // this.onStop = this.onStop.bind(this);
 
     this.input.addEventListener('keydown', this.onSubmit);
-    this.audioButton.addEventListener('click', this.onAudio);
+    // this.audioButton.addEventListener('click', this.onAudio);
     // this.videoButton.addEventListener('click', this.onVideo);
     // this.mediaStop.addEventListener('click', this.onStop);
   }
@@ -34,65 +34,61 @@ export default class App {
       if (text.trim().length === 0) {
         return;
       }
-      const obj = {
-        id: 0,
-        text,
-      };
-      this.memory.addPost(obj);
+      this.memory.addPost(text);
       this.form.reset();
     }
   }
 
-  onAudio(e) {
-    e.preventDefault();
+  // onAudio(e) {
+  //   e.preventDefault();
 
-    const audioStream = this.element.querySelector('.audio-stream');
-    const audioPlayer = audioStream.querySelector('.audio-player');
-    const mediaStart = audioStream.querySelector('.media-start');
-    const mediaStop = audioStream.querySelector('.media-stop');
-    const obj = {
-      id: 1,
-    };
-    const audioPost = Memory.createPost(obj);
+  //   const audioStream = this.element.querySelector('.audio-stream');
+  //   const audioPlayer = audioStream.querySelector('.audio-player');
+  //   const mediaStart = audioStream.querySelector('.media-start');
+  //   const mediaStop = audioStream.querySelector('.media-stop');
+  //   const obj = {
+  //     id: 1,
+  //   };
+  //   const audioPost = Memory.createPost(obj);
 
-    this.form.classList.toggle('hidden');
-    audioStream.classList.toggle('hidden');
+  //   this.form.classList.toggle('hidden');
+  //   audioStream.classList.toggle('hidden');
 
-    mediaStart.addEventListener('click', async () => {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-      });
+  //   mediaStart.addEventListener('click', async () => {
+  //     const stream = await navigator.mediaDevices.getUserMedia({
+  //       audio: true,
+  //     });
 
-      const recorder = new MediaRecorder(stream);
-      const chunks = [];
+  //     const recorder = new MediaRecorder(stream);
+  //     const chunks = [];
 
-      recorder.addEventListener('start', () => {
-        console.log('start');
-      });
+  //     recorder.addEventListener('start', () => {
+  //       console.log('start');
+  //     });
 
-      recorder.addEventListener('dataavailable', (event) => {
-        chunks.push(event.data);
-      });
+  //     recorder.addEventListener('dataavailable', (event) => {
+  //       chunks.push(event.data);
+  //     });
 
-      recorder.addEventListener('stop', () => {
-        const blob = new Blob(chunks);
-        audioPlayer.srcObj = URL.createObjectURL(blob);
-      });
+  //     recorder.addEventListener('stop', () => {
+  //       const blob = new Blob(chunks);
+  //       audioPlayer.srcObj = URL.createObjectURL(blob);
+  //     });
 
-      recorder.start();
+  //     recorder.start();
 
-      mediaStart.addEventListener('click', () => {
-        recorder.stop();
-        stream.getTracks().forEach((track) => track.stop());
-      });
-    });
-  }
+  //     mediaStart.addEventListener('click', () => {
+  //       recorder.stop();
+  //       stream.getTracks().forEach((track) => track.stop());
+  //     });
+  //   });
+  // }
 
-//   onVideo(e) {
-//     e.preventDefault();
+  // onVideo(e) {
+  //   e.preventDefault();
 
-//     this.form.classList.toggle('hidden');
-//     this.media.classList.add('video');
-//     this.media.classList.toggle('hidden');
-//   }
+  //   this.form.classList.toggle('hidden');
+  //   this.media.classList.add('video');
+  //   this.media.classList.toggle('hidden');
+  // }
 }
--- src/components/memory/Memory.js
+++ src/components/memory/Memory.js
@@ -2,77 +2,55 @@ import moment from 'moment/moment';
 
 export default class Memory {
   constructor() {
-    this.memory = [];
-    this.coords = null;
+    this.wrapper = document.querySelector('.wrapper');
   }
 
   static createPost(obj) {
-    let html;
-    if (obj.id === 0) {
-      html = `
-        <div class="post text-post">
-          <p class="text">${obj.text}</p>
-          <time class="time">${obj.time}</time>
-          <footer class="coordinates">${obj.coordinates}</footer>
-        </div>
-      `;
-    }
-    // if (obj.id === 1) {
-    //   html = `
-    //     <div class="post audio-post">
-    //       <audio class="audio-player" controls></audio>
-    //       <p class="time">${obj.time}</p>
-    //       <span class="coordinates">${obj.coordinates}</span>
-    //     </div>
-    //   `;
-    // }
+    const html = `
+      <div class="post text-post">
+        <p class="text">${obj.text}</p>
+        <time class="time">${obj.time}</time>
+        <footer class="coordinates">${obj.coordinates}</footer>
+      </div>
+    `;
     return html;
   }
 
-  addPost(obj) {
-    let post;
-    const coords = Memory.getCoords();
-    if (obj.id === 0) {
-      post = {
-        id: obj.id,
-        text: obj.text,
+  async addPost(text) {
+    let coords = await Memory.getCoords();
+    if (coords) {
+      const obj = {
+        text,
         time: Memory.getTime(),
         coords,
       };
-    } else if (obj.id === 1) {
-      post = {
-        id: obj.id,
+      const post = Memory.createPost(obj);
+      this.wrapper.insertAdjacentHTML('afterbegin', post);
+    } else {
+      coords = await Memory.askForCoords();
+      const obj = {
+        text,
         time: Memory.getTime(),
-        coordinates: obj.coords,
+        coords,
       };
+      const post = Memory.createPost(obj);
+      this.wrapper.insertAdjacentHTML('afterbegin', post);
     }
-    this.memory.push(post);
-    this.render();
   }
 
   static getTime() {
     return moment().format('DD.MM.YYYY HH:mm');
   }
 
-  static getCoords() {
+  static async getCoords() {
     let coords;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((data) => {
         const { latitude, longitude } = data.coords;
         coords = `${latitude}, ${longitude}`;
-      }, (err) => {
-        try {
-          coords = Memory.askForCoords();
-        } catch (e) {
-          console.log(err, e);
-        }
+      }, async (err) => {
+        console.log(err);
       });
-    } else {
-      try {
-        coords = Memory.askForCoords();
-      } catch (e) {
-        console.log(e);
-      }
     }
     return coords;
   }
@@ -93,25 +71,11 @@ export default class Memory {
       coords = cleanData;
       popupForm.reset();
       popup.classList.add('hidden');
-      return coords || null;
+      return coords;
     });
     closeButton.addEventListener('click', () => {
       popupForm.reset();
       popup.classList.add('hidden');
-    });
-  }
-
-  static clearDOM() {
-    const posts = document.querySelectorAll('.post');
-    posts.forEach((item) => item.remove());
-  }
-
-  render() {
-    Memory.clearDOM();
-    const wrapper = document.querySelector('.wrapper');
-    this.memory.forEach((item) => {
-      const post = Memory.createPost(item);
-      wrapper.insertAdjacentHTML('afterbegin', post);
     });
   }
 }
